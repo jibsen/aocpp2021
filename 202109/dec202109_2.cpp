b28b2ebc512344c2d6979b1589266a66f3bac2e1
Joergen Ibsen <ji@ibse.dk>
2021-12-09 11:40:17 +0100

    Day 9 part two

---
+++ 202109/dec202109_2.cpp
@@ -0,0 +1,75 @@
+//
+// Advent of Code 2021, day 9, part two
+//
+
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <string>
+#include <utility>
+#include <vector>
+
+int fill_basin(std::vector<std::string> &map, int start_x, int start_y)
+{
+	const std::array<std::array<int, 2>, 4> directions = {{
+		{ 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 }
+	}};
+
+	std::queue<std::pair<int, int>> queue;
+
+	queue.emplace(start_x, start_y);
+
+	int size = 0;
+
+	while (!queue.empty()) {
+		auto [x, y] = queue.front();
+		queue.pop();
+
+		if (map[y][x] == '9') {
+			continue;
+		}
+
+		map[y][x] = '9';
+		++size;
+
+		for (auto [dx, dy] : directions) {
+			if (x + dx < 0 || x + dx >= map[y].size()
+			 || y + dy < 0 || y + dy >= map.size()) {
+				continue;
+			}
+
+			queue.emplace(x + dx, y + dy);
+		}
+	}
+
+	return size;
+}
+
+int main()
+{
+	std::vector<std::string> map(std::istream_iterator<std::string>{std::cin},
+	                             std::istream_iterator<std::string>{});
+
+	std::vector<int> sizes;
+
+	for (int y = 0; y < map.size(); ++y) {
+		for (int x = 0; x < map[y].size(); ++x) {
+			if (map[y][x] == '9') {
+				continue;
+			}
+
+			auto size = fill_basin(map, x, y);
+
+			sizes.push_back(size);
+		}
+	}
+
+	std::ranges::partial_sort(sizes.begin(), sizes.begin() + 3, sizes.end(), std::greater{});
+
+	std::cout << std::accumulate(sizes.begin(), sizes.begin() + 3, 1, std::multiplies{}) << '\n';
+}
